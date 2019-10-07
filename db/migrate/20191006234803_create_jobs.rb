4f3aa739947564a52e01dfe0ba4163b5dc3359b2
Dianna Tingg <dtingg@gmail.com>
2019-10-06 18:21:44 -0700

    added job model and migration

---
+++ db/migrate/20191006234803_create_jobs.rb
@@ -0,0 +1,10 @@
+class CreateJobs < ActiveRecord::Migration[5.2]
+  def change
+    create_table :jobs do |t|
+      t.string :title
+      t.string :description
+      t.string :company
+      t.timestamps
+    end
+  end
+end
